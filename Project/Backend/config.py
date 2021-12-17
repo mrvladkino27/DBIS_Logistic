c7869b7ef4e9c3d2978eb34ba85af6313b296f94
mrvladkino27 <mar4vv@gmail.com>
2021-12-17 05:32:03 +0200

    update db connection

---
+++ Project/Backend/config.py
@@ -0,0 +1,15 @@
+from configparser import ConfigParser
+import os
+
+def config(filename, section='postgresql'):
+    if os.path.isfile(filename):
+        parser = ConfigParser()
+        parser.read(filename)
+        db = {}
+        if parser.has_section(section):
+            params = parser.items(section)
+            for param in params:
+                db[param[0]] = param[1]
+        else:
+            raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+        return f'postgresql+psycopg2://{db["user"]}:{db["password"]}@{db["host"]}/{db["database"]}'
